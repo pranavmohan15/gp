327845ac92c7c21846a42626d7502b7f9a7fc8ec
Your Name <pranavmohanindeevaram@gmail.com>
2026-03-03 22:35:02 +0530

    updating

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,5 +1,14 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
+  useGSAP(() => {
+    gsap.to('#blue-box', { x: 800, 
+      repeat: -1, yoyo: true ,
+    rotate: 360,
+  duration:4});
+  }, []);
 
   return (
     <main>
